ac0b7e3b65553f1d9db35b020f48a20a294df31d
Marcos Daniel Romero Garcia <Marck Romero>
2025-03-10 03:01:08 -0500

    feat: Diseño de carrito maquetado

--- src/components/DessertCart.tsx
+++ src/components/DessertCart.tsx
@@ -1,9 +1,13 @@
+import { useMemo } from "react"
 import { useDessertStore } from "../store"
 
 export const DessertCart = () => {
 
-   const dessertCart =  useDessertStore(state => state.dessertCart)
-   const dessertCartQuantity =  dessertCart.length
+
+
+    const dessertCart =   useDessertStore(state => state.dessertCart)
+    const cartPriceTotal = useMemo(() => dessertCart.reduce((acc, i) =>  acc + (i.price  * i.quantity), 0 ).toFixed(2) , [dessertCart])
+    const dessertCartQuantity =  useMemo(() => dessertCart.reduce((acc, i) => acc + (i.quantity), 0), [dessertCart]) 
 
   return (
     <>
@@ -11,28 +15,49 @@ export const DessertCart = () => {
             <h2 className="text-m-Red font-bold text-3xl">Your Cart: {dessertCartQuantity}</h2>                
             
         {dessertCart.length > 0? (
-            dessertCart.map((item) => (
-                <section>
-                    <article className="border-b py-4 border-m-Rose100 flex items-center justify-between">
-                        <div className="space-y-1">
-                            <p className="font-semibold">{item.name}</p>
-                            <div className="flex gap-2">
-                                <p className="text-m-Red font-bold pr-4">{item.quantity}x</p>
-                                <p>@ ${item.price.toFixed(2)}</p>
-                                <p className="font-semibold">${(item.price * item.quantity).toFixed(2)}</p>
+            <>
+                {
+                    dessertCart.map((item) => (
+                        <article className="border-b py-4 border-m-Rose100 flex items-center justify-between">
+                            <div className="space-y-1">
+                                <p className="font-semibold">{item.name}</p>
+                                <div className="flex gap-2">
+                                    <p className="text-m-Red font-bold pr-4">{item.quantity}x</p>
+                                    <p>@ ${item.price.toFixed(2)}</p>
+                                    <p className="font-semibold">${(item.price * item.quantity).toFixed(2)}</p>
+                                </div>
+                            </div>
+                            <p className="border w-7 h-7 text-2xl flex justify-center items-center rounded-full text-center cursor-pointer hover:bg-m-Rose900 transition-all duration-150 hover:text-m-Rose100 hover:border-m-Rose900 m-0">
+                                ×
+                            </p>
+                        </article>      
+                    ))
+                }
+
+                {
+                    dessertCartQuantity !== 0 ? (
+                        <div className="mb-4">
+                            <div className="flex justify-between py-6 items-center">
+                                <p>Order Total</p>
+                                <p className="font-bold text-2xl">${cartPriceTotal}</p>
+                            </div>
+
+                            <div className="flex justify-center gap-2 bg-m-Rose100 py-4 rounded">
+                                <img src="/assets/images/icon-carbon-neutral.svg" alt="arbol" />
+                                <p>This is a <strong>carbon-neutral</strong> delivery</p>
                             </div>
                         </div>
-                        <p className="border w-7 h-7 text-2xl flex justify-center items-center rounded-full text-center cursor-pointer hover:bg-m-Rose900 transition-all duration-150 hover:text-m-Rose100 hover:border-m-Rose900 m-0">
-                            ×
-                        </p>
-                    </article>
-                    <section>
-                        <p>Order Total</p>
-                        <p></p>
-                    </section>
-                </section>
-                
-            ))
+
+                    ) : (
+                        null
+                    )
+                }
+
+                <button className="bg-m-Red transition-all duration-150 block w-full text-m-Rose50 py-4 rounded-4xl font-semibold cursor-pointer hover:bg-m-Red-hover">
+                    Confirm Order
+                </button>
+            </>
+            
         ): (
             <div className="flex flex-col justify-center items-center gap-4 py-8">
                     <img src="/assets/images/illustration-empty-cart.svg" alt="Empty cart" />
